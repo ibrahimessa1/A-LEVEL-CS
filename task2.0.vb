7165baf4c314bcc8e481b90538db6f8e364c83ef
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-02-04 17:47:10 +0500

    Add files via upload

---
+++ task2.0.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Count As Integer
+        Console.Write("Enter number of donuts: ")
+        Count = Console.ReadLine()
+        If Count < 10 Then
+            Console.WriteLine("Number of donuts: " & Count)
+        Else : Console.WriteLine("Number of donuts: Many")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
