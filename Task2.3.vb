b28e2d37d48ced043bc5eda26a614da2789e19c9
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-02-04 18:03:04 +0500

    Add files via upload

---
+++ Task2.3.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim first As String
+        Dim second As String
+        Dim updatedS As String = ""
+        Console.Write("Enter first string: ")
+        first = Console.ReadLine()
+        Console.Write("Enter second string: ")
+        second = Console.ReadLine()
+        updatedS = Mid(second, 1, 2) & Mid(first, 3) & " " & Mid(first, 1, 2) & Mid(second, 3)
+        Console.WriteLine(updatedS)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
