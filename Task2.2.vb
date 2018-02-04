a0fd182a38dcbb333b9371b0f780ac811239c496
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-02-04 17:58:23 +0500

    Add files via upload

---
+++ Task2.2.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim s As String
+        Dim first As Char
+        Dim symbol As Char = "*"
+        Dim updatedS As String
+        Console.Write("Enter string: ")
+        s = Console.ReadLine()
+        first = s(0)
+        updatedS = first
+        For i = 1 To Len(s) - 1
+            If s(i) <> first Then
+                updatedS = updatedS & s(i)
+            Else : updatedS = updatedS & symbol
+            End If
+        Next
+        Console.WriteLine(updatedS)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
