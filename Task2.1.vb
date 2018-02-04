0ba9aaf132e170e1d695af465d2921c4b8399bce
ibrahimessa1 <35236104+ibrahimessa1@users.noreply.github.com>
2018-02-04 17:50:58 +0500

    Add files via upload

---
+++ Task2.1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim text As String
+        Console.Write("Enter string: ")
+        text = Console.ReadLine
+        If Len(text) < 2 Then
+            Console.WriteLine("")
+        Else : Console.WriteLine(Left(text, 2) & Right(text, 2))
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
